fb6e4968780c6f953c108dcd466f4e62893c93ef
Michael Hoffman <michael.hoffman@unc.edu>
2017-03-26 15:05:18 -0400

    updated to make all the data numerical, and predict an age

--- titanic_twomodels.py
+++ titanic_twomodels.py
@@ -16,11 +16,111 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing
 
-training_set = pd.read_csv('train.csv')
-test_set = pd.read_csv('test.csv')
+training_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
 
-sns.pointplot(x="Pclass", y="Survived", hue="Sex", data=training_set,
-              palette={"male": "blue", "female": "pink"},
-              markers=["*", "o"], linestyles=["-", "--"]);
-plt.show()
+
+## Set of functions to transform features into more convenient format.
+#
+# Code performs three separate tasks:
+#   (1). Pull out the first letter of the cabin feature.
+#          Code taken from: https://www.kaggle.com/jeffd23/titanic/scikit-learn-ml-from-start-to-finish
+#   (2). Add column which is binary variable that pertains
+#        to whether the cabin feature is known or not.
+#        (This may be relevant for Pclass = 1).
+#   (3). Recasts cabin feature as number.
+def simplify_cabins(data):
+    data.Cabin = data.Cabin.fillna('N')
+    data.Cabin = data.Cabin.apply(lambda x: x[0])
+
+    cabin_mapping = {'N': 0, 'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1,
+                     'F': 1, 'G': 1, 'T': 1}
+    data['Cabin_Known'] = data.Cabin.map(cabin_mapping)
+
+    le = preprocessing.LabelEncoder().fit(data.Cabin)
+    data.Cabin = le.transform(data.Cabin)
+
+    return data
+
+
+# Recast sex as numerical feature.
+def simplify_sex(data):
+    sex_mapping = {'male': 0, 'female': 1}
+    data.Sex = data.Sex.map(sex_mapping).astype(int)
+
+    return data
+
+
+# Recast port of departure as numerical feature.
+def simplify_embark(data):
+    # Two missing values, assign the most common port of departure.
+    data.Embarked = data.Embarked.fillna('S')
+
+    le = preprocessing.LabelEncoder().fit(data.Embarked)
+    data.Embarked = le.transform(data.Embarked)
+
+    return data
+
+
+# Extract title from names, then assign to one of five ordinal classes.
+# Function based on code from: https://www.kaggle.com/startupsci/titanic/titanic-data-science-solutions
+def add_title(data):
+    data['Title'] = data.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+    data.Title = data.Title.replace(['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major',
+                                     'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    data.Title = data.Title.replace('Mlle', 'Miss')
+    data.Title = data.Title.replace('Ms', 'Miss')
+    data.Title = data.Title.replace('Mme', 'Mrs')
+
+    # Map from strings to ordinal variables.
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+
+    data.Title = data.Title.map(title_mapping)
+    data.Title = data.Title.fillna(0)
+
+    return data
+
+
+# Drop all unwanted features (name, ticket).
+def drop_features(data):
+    return data.drop(['Name', 'Ticket'], axis=1)
+
+
+# Perform all feature transformations.
+def transform_all(data):
+    data = simplify_cabins(data)
+    data = simplify_sex(data)
+    data = simplify_embark(data)
+    data = add_title(data)
+    data = drop_features(data)
+
+    return data
+
+
+training_data = transform_all(training_data)
+test_data = transform_all(test_data)
+all_data = [training_data, test_data]
+combine = pd.concat(all_data)
+
+### age imputation
+entries_with_age = combine[pd.notnull(combine['Age'])]
+entries_no_age = combine[pd.isnull(combine['Age'])]
+entries_with_age = entries_with_age.dropna()
+# logistic regression to predict age
+#entries_with_age = entries_with_age[['Age','Embarked', 'Pclass', 'Sex','Fare','SibSp','Parch','Survived','Title']]
+age_train_x = entries_with_age.drop('Age', axis=1)[:-100]
+age_test_x = entries_with_age.drop('Age', axis=1)[-100:]
+age_train_y = entries_with_age['Age'][:-100]
+age_test_y = entries_with_age['Age'][-100:]
+
+lr_age = LinearRegression()
+lr_age.fit(age_train_x, age_train_y)
+
+# The coefficients
+print(lr_age.coef_)
+
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % lr_age.score(age_test_x, age_test_y))
