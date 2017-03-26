c98be87452947ed4d5b87b9bc9b0f244a1b56c03
Michael Hoffman <michael.hoffman@unc.edu>
2017-03-26 09:02:31 -0400

    initial code changes updated readme with current plan

--- titanic_twomodels.py
+++ titanic_twomodels.py
@@ -1,12 +1,26 @@
-import numpy as np
+# data analysis and wrangling
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+import random as rnd
+
+# visualization
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-data_train = pd.read_csv('train.csv')
-data_test = pd.read_csv('test.csv')
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
 
-sns.pointplot(x="Pclass", y="Survived", hue="Sex", data=data_train,
+training_set = pd.read_csv('train.csv')
+test_set = pd.read_csv('test.csv')
+
+sns.pointplot(x="Pclass", y="Survived", hue="Sex", data=training_set,
               palette={"male": "blue", "female": "pink"},
               markers=["*", "o"], linestyles=["-", "--"]);
 plt.show()
